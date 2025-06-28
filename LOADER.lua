5c2ca82f0abc56c1f143dc7f70612cd0423df80b
water.5202 <yeahidkorb@gmail.com>
2025-06-28 20:13:25 +0100

    LOADER.lua

--- LOADER.lua
+++ LOADER.lua
@@ -231,8 +231,6 @@ game:GetService("Players").LocalPlayer.PlayerGui.FunnyCatShop.Sh.Balance.heya.Te
 end
 end)
 
-game:GetService("Players").LocalPlayer.PlayerGui.BetterHealthBar.HealthGui
-
 local HideHP = Tabs.Utils:AddToggle("Hides Health bar", {Title = "Hide Health Bar", Default = nil })
 
 HideHP:OnChanged(function(Value)
