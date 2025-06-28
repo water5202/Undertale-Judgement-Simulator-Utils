e8a5524a72beb470438c861a5e79bc5bf5999b5f
water.5202 <yeahidkorb@gmail.com>
2025-06-28 18:51:39 +0100

    LOADER.lua

--- LOADER.lua
+++ LOADER.lua
@@ -49,6 +49,20 @@ local lobbytp = Tabs.Utils:AddButton({
                         if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
                             local hrp = player.Character:FindFirstChild("HumanoidRootPart")
                             hrp.CFrame = CFrame.new(64.5012894, 4.00011635, -124.729019, -0.00950993411, 0, 0.99995476, 0, 1, 0, -0.99995476, 0, -0.00950993411)
+                        task.wait(1)
+                        Fluent:Notify({
+        Title = "UJS [Interface]",
+        Content = "Teleported to lobby",
+        SubContent = "",
+        Duration = 5
+})
+                        else
+                        Fluent:Notify({
+        Title = "UJS [Interface]",
+        Content = "Failed to Teleport",
+        SubContent = "",
+        Duration = 5
+})
                         end
                     },
                     {
