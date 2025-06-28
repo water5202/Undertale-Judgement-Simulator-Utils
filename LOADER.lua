787d56a59523989c2bb2e7beeeb9674368a84308
water.5202 <yeahidkorb@gmail.com>
2025-06-28 18:54:17 +0100

    LOADER.lua

--- LOADER.lua
+++ LOADER.lua
@@ -35,42 +35,48 @@ CurrentPlayers:SetTitle(currentplayer .. " │ players")
 end)
 
 local lobbytp = Tabs.Utils:AddButton({
-        Title = "Lobby TP",
-        Description = "Lobby Teleport",
-        Callback = function()
-            Window:Dialog({
-                Title = "Teleport to Lobby?",
-                Content = nil,
-                Buttons = {
-                    {
-                        Title = "Confirm",
-                        Callback = function()
+    Title = "Lobby TP",
+    Description = "Lobby Teleport",
+    Callback = function()
+        Window:Dialog({
+            Title = "Teleport to Lobby?",
+            Content = nil,
+            Buttons = {
+                {
+                    Title = "Confirm",
+                    Callback = function()
                         local player = game.Players.LocalPlayer
                         if player and player.Character and player.Character:FindFirstChild("HumanoidRootPart") then
                             local hrp = player.Character:FindFirstChild("HumanoidRootPart")
-                            hrp.CFrame = CFrame.new(64.5012894, 4.00011635, -124.729019, -0.00950993411, 0, 0.99995476, 0, 1, 0, -0.99995476, 0, -0.00950993411)
-                        task.wait(1)
-                        Fluent:Notify({
-        Title = "UJS [Interface]",
-        Content = "Teleported to lobby",
-        SubContent = "",
-        Duration = 5
-})
+                            hrp.CFrame = CFrame.new(
+                                64.5012894, 4.00011635, -124.729019,
+                                -0.00950993411, 0, 0.99995476,
+                                0, 1, 0,
+                                -0.99995476, 0, -0.00950993411
+                            )
+                            task.wait(1)
+                            Fluent:Notify({
+                                Title = "UJS [Interface]",
+                                Content = "Teleported to lobby",
+                                SubContent = "",
+                                Duration = 5
+                            })
                         else
-                        Fluent:Notify({
-        Title = "UJS [Interface]",
-        Content = "Failed to Teleport",
-        SubContent = "",
-        Duration = 5
-})
+                            Fluent:Notify({
+                                Title = "UJS [Interface]",
+                                Content = "Failed to Teleport",
+                                SubContent = "",
+                                Duration = 5
+                            })
                         end
-                    },
-                    {
-                        Title = "Cancel",
-                        Callback = function()
-                        end
-                    }
+                    end
+                },
+                {
+                    Title = "Cancel",
+                    Callback = function()
+                    end
                 }
-            })
-        end
-    })
+            }
+        })
+    end
+})
