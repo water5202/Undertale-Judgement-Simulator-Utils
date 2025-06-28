70081cba923b235d74f9973cdb20d6cb6e618bf7
water.5202 <yeahidkorb@gmail.com>
2025-06-28 18:36:01 +0100

    LOADER.lua

--- LOADER.lua
+++ LOADER.lua
@@ -1,4 +1,10 @@
 local Library = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+Fluent:Notify({
+        Title = "UJS [Interface]",
+        Content = "1.0",
+        SubContent = "made by water.5202 :D", -- Optional
+        Duration = 5 -- Set to nil to make the notification not disappear
+})
 
 local Window = Fluent:CreateWindow({
     Title = "UJS [Water's Utils] : " .. identifyexecutor(),
