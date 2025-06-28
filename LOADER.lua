c40812fd3fa9b8e07adddb2df9e947473101babf
water.5202 <yeahidkorb@gmail.com>
2025-06-28 18:35:14 +0100

    LOADER.lua

--- LOADER.lua
+++ LOADER.lua
@@ -1,1 +1,11 @@
+local Library = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
+local Window = Fluent:CreateWindow({
+    Title = "UJS [Water's Utils] : " .. identifyexecutor(),
+    SubTitle = "by dawid",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = true,
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.Quote
+})
