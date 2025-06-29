3163fa396ddf3014443e5be862e8c46f038b7b26
water.5202 <yeahidkorb@gmail.com>
2025-06-29 12:34:27 +0100

    LOADER.lua

--- LOADER.lua
+++ LOADER.lua
@@ -294,34 +294,23 @@ local EspToggle = Tabs.Visuals:AddToggle("ESPVAL", {
 
 local WeaponSelect = Tabs.Utilities:AddDropdown("ItemDrop", {
     Title = "Weapon Select",
-    Description = nil,
+    Description = "Select a weapon to auto-pick",
     Values = {"Stick", "Toy Knife", "Torn Notebook", "Burnt Pan", "Real Knife", "Jolly Dagger"},
     Multi = false,
-    Default = nil,
+    Default = "Stick",
+    Callback = function(Value)
+        if Value == "Stick" then
+            fireclickdetector(workspace.WeaponBlocks.Stick.ClickDetector)
+        elseif Value == "Toy Knife" then
+            fireclickdetector(workspace.WeaponBlocks.ToyKnife.ClickDetector)
+        elseif Value == "Torn Notebook" then
+            fireclickdetector(workspace.WeaponBlocks.TornNotebook.ClickDetector)
+        elseif Value == "Burnt Pan" then
+            fireclickdetector(workspace.WeaponBlocks.BurntPan.ClickDetector)
+        elseif Value == "Real Knife" then
+            fireclickdetector(workspace.WeaponBlocks.RealKnife.ClickDetector)
+        elseif Value == "Jolly Dagger" then
+            fireclickdetector(workspace.WeaponBlocks.JollyDagger.ClickDetector)
+        end
+    end
 })
-
-WeaponSelect:OnChanged(function(Value)
-if Value == "Stick" then
-fireclickdetector(workspace.WeaponBlocks.Stick.ClickDetector)
-end
-
-if Value == "Toy Knife" then
-fireclickdetector(workspace.WeaponBlocks.ToyKnife.ClickDetector)
-end
-
-if Value == "Torn Notebook" then
-fireclickdetector(workspace.WeaponBlocks.TornNotebook.ClickDetector)
-end
-
-if Value == "Burnt Pan" then
-fireclickdetector(workspace.WeaponBlocks.BurntPan.ClickDetector)
-end
-
-if Value == "Real Knife" then
-fireclickdetector(workspace.WeaponBlocks.RealKnife.ClickDetector)
-end
-
-if Value == "Jolly Dagger" then
-fireclickdetector(workspace.WeaponBlocks.JollyDagger.ClickDetector)
-end
-end)
